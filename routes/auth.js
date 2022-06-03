53028b7cb37d6e63675f40aae7fd7cc258632689
Utkarsh <utkarshyadavup22@gmail.com>
2022-06-03 11:40:29 +0530

    adding endpoint to add notes and fetch notes :) and rename file Notes to Note

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator")
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const fetchuser = require("../middleware/fetchuser")
+const fetchuser = require("../middleware/fetchuser");
 const jwt_SECRET = 'MYNOTEBOOKISBESTUTKARSHYADAV';
 
 
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
+// Route 1: fetch user all notes using:  GET  "/api/auth/fetchallnotes". no login required
 
-router.get('/', (req, res)=>{
-    obj = []
-    res.json(obj);
+router.get('/fetchallnotes',    fetchuser,  async (req, res)=>{
+
+    try {
+        const notes = await Note.find({user: req.user.id});
+        res.json(notes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Some error occured");
+    }
+
 });
 
+
+// Route 2: Adding user notes using:  POST  "/api/auth/addnote". no login required
+router.post('/addnote',     fetchuser,      [
+        body('title', "Enter a valid title").isLength({min: 3}),
+        body('description', "description must be atleast 5 characters").isLength({min: 5})
+], async (req, res)=>{
+    
+    try {
+
+    let {title, description, tag} = req.body;
+    // If there are errors. return Bad request and bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() });
+    }
+
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        });
+        const saveNote = await note.save();
+
+        res.json(saveNote);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Some error occured");
+}
+}  );
 module.exports = router;
